ba64a6ea926840e0b32f19dc7b0bdea274d79fa5
GABRIEL <angelgab43>
2021-12-17 15:39:21 +0300

    first commit

---
+++ start_miner_algo.bat
@@ -0,0 +1,14 @@
+REM
+REM Example bat file for starting PhoenixMiner.exe to mine ETH
+REM
+
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+REM IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+PhoenixMiner.exe -pool ethash.unmineable.com:3333 -wal ALGO:0x71447142a4C626A7b9ca9Eb8869Ad1840d9cF7c0.MaticMiner01#yiav-fvvq -pass x 
+pause
+
